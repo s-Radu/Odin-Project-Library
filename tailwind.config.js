118434b2a0cc3c2e07474b77d69daed92495c0b3
lil.void <tsraduu@gmail.com>
2023-12-14 19:55:09 +0000

    Library project boilerplate done

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,15 @@ module.exports = {
   content: ["./**/*.{html,js}"],
   purge: ["./**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        josefin: '"Josefin Sans", sans-serif',
+        montserat: '"Montserrat", sans-serif',
+        nunito: '"Nunito", sans-serif',
+      },
+    },
   },
   plugins: [],
 };
+
+//? npx tailwindcss -i ./src/input.css -o ./src/output.css --watch
