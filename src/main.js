04b078f73725fe8269fca4ba0bee635050df1b9a
lil.void <tsraduu@gmail.com>
2023-12-15 20:33:07 +0000

    Slide in animation done, now slide out needs to be done

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 //? Global variables
 
 const bookDetailsBtn = document.querySelectorAll(".more");
+const hodeBookDetailsBtn = document.querySelectorAll(".less");
 
 //* Functions
 
@@ -8,13 +9,48 @@ function showBookDetails(e) {
   const cardElement = e.target.closest(".card");
   const readButton = cardElement.querySelector(".read");
   const notReadButton = cardElement.querySelector(".not-read");
+  const cardContent = cardElement.querySelector(".card-content");
+  const bookDetails = cardElement.nextElementSibling;
+
   readButton.disabled = true;
   notReadButton.disabled = true;
+  cardContent.classList.add("exits");
+  bookDetails.classList.add("enters");
+
+  setTimeout(() => {
+    cardContent.classList.add("hidden");
+    bookDetails.classList.remove("hidden");
+  }, 800);
+
   cardElement.classList.add("blur-md");
+}
+
+function hideBookDetails(e) {
+  const cardElement = e.target.closest(".card");
+  const readButton = cardElement.querySelector(".read");
+  const notReadButton = cardElement.querySelector(".not-read");
+  const cardContent = cardElement.querySelector(".card-content");
+  const bookDetails = cardElement.nextElementSibling;
+
+  readButton.disabled = false;
+  notReadButton.disabled = false;
+  cardContent.classList.remove("exits");
+  bookDetails.classList.remove("enters");
+
+  setTimeout(() => {
+    cardContent.classList.remove("hidden");
+    bookDetails.classList.add("hidden");
+  }, 800);
+
+  cardElement.classList.remove("blur-md");
 }
 
 //! Event listeners
 
 bookDetailsBtn.forEach((btn) => {
   btn.addEventListener("click", showBookDetails);
+});
+
+hodeBookDetailsBtn.forEach((btn) => {
+  btn.addEventListener("click", hideBookDetails);
 });
