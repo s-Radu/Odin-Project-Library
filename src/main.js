dc08abb1eee4fc10e0ae962db354bd88d26b077a
lil.void <tsraduu@gmail.com>
2023-12-16 13:33:57 +0000

    Managed to add the newly created books to the dom

--- src/main.js
+++ src/main.js
@@ -15,6 +15,10 @@ const bookDescriptionInput = document.getElementById("description");
 const pagesNumberInput = document.getElementById("pages");
 const readBookInput = document.getElementById("book-read");
 
+//* Read and not read sections
+const readSection = document.getElementById("read");
+const notReadSection = document.getElementById("not-read");
+
 //> Book array
 let readLibrary = [];
 let notReadLibrary = [];
@@ -63,10 +67,13 @@ function addBookToLibrary(e) {
 
     if (readBook === "yes") {
       readLibrary.push(newBook);
+      let lastReadLibraryBook = readLibrary[readLibrary.length - 1];
+      createBookCard(lastReadLibraryBook, true);
     } else if (readBook === "no") {
       notReadLibrary.push(newBook);
+      let lastNotReadLibraryBook = notReadLibrary[notReadLibrary.length - 1];
+      createBookCard(lastNotReadLibraryBook, false);
     }
-    console.log(readBook);
 
     clearDialog();
     dialog.close();
@@ -120,8 +127,6 @@ function clearDialog() {
   pagesNumberInput.value = "";
 }
 
-submitBookBtn.addEventListener("click", addBookToLibrary);
-
 function showBookDetails(e) {
   const cardElement = e.target.closest(".card");
   const readButton = cardElement.querySelector(".read");
@@ -171,6 +176,107 @@ function hideBookDetails(e) {
   }
 }
 
+function createBookCard(book, isRead) {
+  const newBookCard = document.createElement("div");
+
+  newBookCard.innerHTML = `
+  <div class="p-4 transition ease-in-out delay-75 duration-200 min-h-card relative book ">
+  <div class="h-full border-2 border-gray-200 border-opacity-60 rounded-lg overflow-hidden card">
+
+      <div class="h-full p-6 flex flex-col justify-between card-content">
+          <div class="book-info">
+              <h1 class="text-5xl text-center m-10 border-b-2 border-gray-400 p-1">Book title</h1>
+              <h2 class="tracking-widest text-center text-xl font-medium text-gray-200 mb-3">Author</h2>
+              <h2
+                  class="tracking-widest text-center text-2xl font-medium text-gray-200 mb-1 border-b-2 border-gray-400">
+                  J. K. Rowlings</h2>
+          </div>
+          <div class="flex justify-between m-2">
+              <p class="text-sm text-left"> Pages: </p>
+              <p class="number-of-pages text sm text-right">64s</p>
+          </div>
+
+          <div class="quote-section border-2 border-gray-400 rounded-2xl m-2 mt-10">
+              <h2 class="text-center text-2xl font-medium text-gray-200 mb-1 border-gray-400 m-2 ">
+                  Quote</h2>
+              <p class="text-left indent-6 pb-4 m-4 font-medium text-gray-200 mb-1 border-gray-400 italic font-nunito
+              before:quote-content after:quote-content ">
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore eligendi deserunt qui
+                  beatae provident consectetur eveniet error laudantium, doloribus sit!
+              </p>
+          </div>
+
+          <div class="flex flex-col">
+              <div class="flex gap-4  mx-auto justify-around align-center m-10 ">
+                  <button
+                      class="read bg-blue-500 hover:bg-blue-400 text-white font-bold py-2 px-4 border-b-4 border-blue-700 hover:border-blue-500 rounded w-20">Read</button>
+                  <button
+                      class="not-read  bg-red-500 hover:bg-red-400 text-white font-bold py-2 px-4 border-b-4 border-red-700 hover:border-red-500 rounded w-20  ">Remove</button>
+              </div>
+
+              <div class="flex items-center justify-self-end">
+                  <a class="text-indigo-500 inline-flex items-center md:mb-2 lg:mb-0 cursor-pointer more">
+                      More
+                      <svg class="w-4 h-4 ml-2" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2"
+                          fill="none" stroke-linecap="round" stroke-linejoin="round">
+                          <path d="M5 12h14"></path>
+                          <path d="M12 5l7 7-7 7"></path>
+                      </svg>
+                  </a>
+                  <span
+                      class="text-gray-400 mr-3 inline-flex items-center lg:ml-auto md:ml-0 ml-auto leading-none text-sm pr-3 py-1 border-r-2 border-gray-200">
+                      <svg class="w-4 h-4 mr-1" stroke="currentColor" stroke-width="2" fill="none"
+                          stroke-linecap="round" stroke-linejoin="round" viewBox="0 0 24 24">
+                          <path d="M1 12s4-8 11-8 11 8 11 8-4 8-11 8-11-8-11-8z"></path>
+                          <circle cx="12" cy="12" r="3"></circle>
+                      </svg>1.2K
+                  </span>
+                  <span class="text-gray-400 inline-flex items-center leading-none text-sm">
+                      <svg class="w-4 h-4 mr-1" stroke="currentColor" stroke-width="2" fill="none"
+                          stroke-linecap="round" stroke-linejoin="round" viewBox="0 0 24 24">
+                          <path
+                              d="M21 11.5a8.38 8.38 0 01-.9 3.8 8.5 8.5 0 01-7.6 4.7 8.38 8.38 0 01-3.8-.9L3 21l1.9-5.7a8.38 8.38 0 01-.9-3.8 8.5 8.5 0 014.7-7.6 8.38 8.38 0 013.8-.9h.5a8.48 8.48 0 018 8v.5z">
+                          </path>
+                      </svg>6
+                  </span>
+              </div>
+          </div>
+      </div>
+  </div>
+  <div class="flex flex-col h-full justify-center items-center m-4  book-details hidden absolute top-0 ">
+      <p class="first-line:uppercase first-line:tracking-widest
+      first-letter:text-7xl first-letter:font-bold
+      first-letter:mr-3 first-letter:float-left m-4
+    ">
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Asperiores maxime, quibusdam
+          voluptates ab in ducimus illum numquam fugit tempora, cupiditate alias perspiciatis saepe
+          qui
+          explicabo, possimus illo? Incidunt, perferendis magni. Veritatis possimus quam illum at
+          expedita? A iure nulla inventore, ipsum possimus tenetur optio. Sed, deleniti. Minus
+          nesciunt
+          pariatur similique id. Magni recusandae facilis exercitationem dolores culpa consequuntur
+          dolor
+          nisi!
+      </p>
+      <a class="text-indigo-500 inline-flex items-center md:mb-2 lg:mb-0 cursor-pointer less">
+          Less
+          <svg class="w-4 h-4 ml-2" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2" fill="none"
+              stroke-linecap="round" stroke-linejoin="round">
+              <path d="M12 5v14"></path>
+              <path d="M19 12l-7 7-7-7"></path>
+          </svg>
+      </a>
+  </div>
+</div>
+
+  `;
+  if (isRead) {
+    readSection.appendChild(newBookCard);
+  } else {
+    notReadSection.appendChild(newBookCard);
+  }
+}
+
 //! Event listeners
 
 bookDetailsBtn.forEach((btn) => {
@@ -189,3 +295,5 @@ closeButton.addEventListener("click", () => {
   clearDialog();
   dialog.close();
 });
+
+submitBookBtn.addEventListener("click", addBookToLibrary);
