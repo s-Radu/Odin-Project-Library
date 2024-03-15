e885dd99b27363ee26ed46e39e318affde36bf68
lil void <32060295+s-Radu@users.noreply.github.com>
2024-03-15 19:27:40 +0000

    Update main.js

--- src/main.js
+++ src/main.js
@@ -159,9 +159,9 @@ function createBookCard(book, isRead) {
 
   if (isRead === true) {
     newBookCard.classList.add("read-book");
-    bookRead = "Not Read";
-  } else {
     bookRead = "Read";
+  } else {
+    bookRead = "Not Read";
     newBookCard.classList.add("book-not-read");
   }
 
